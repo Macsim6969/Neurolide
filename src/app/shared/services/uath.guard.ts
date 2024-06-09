4351601fb67bb888def525815b14edf72af660ba
MacsimB <macs.belousov666@gmail.com>
2024-06-09 17:00:17 +0300

    updated auth guard

--- src/app/shared/services/uath.guard.ts
+++ src/app/shared/services/uath.guard.ts
@@ -1,14 +1,20 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (localStorage.getItem('userData')) {
-      const rules = JSON.parse(localStorage.getItem('rules'))
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const userData = localStorage.getItem('userData');
+    if (!userData) {
+      return true
+    } else {
+      const rules = JSON.parse(localStorage.getItem('rules'));
       if (rules === 'manager') {
         return this.router.createUrlTree(['/manager']);
       } else if (rules === 'brand') {
@@ -16,10 +22,8 @@ export class AuthGuard implements CanActivate {
       } else if (rules === 'affiliate') {
         return this.router.createUrlTree(['/affiliate']);
       } else {
-        return false
+        return false;
       }
-    } else {
-      return this.router.createUrlTree(['/register']);
     }
   }
 }
