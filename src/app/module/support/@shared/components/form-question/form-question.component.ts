30a661cee2b8e5a29cb97bf1029293166b008b9b
MacsimB <macs.belousov666@gmail.com>
2024-06-17 10:53:20 +0300

    updated support page

--- src/app/module/support/@shared/components/form-question/form-question.component.ts
+++ src/app/module/support/@shared/components/form-question/form-question.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { Subject, takeUntil } from 'rxjs';
+import { QuestionForm } from '../../interface/questions.interface';
 
 @Component({
   selector: 'app-form-question',
   templateUrl: './form-question.component.html',
   styleUrls: ['./form-question.component.scss']
 })
-export class FormQuestionComponent implements OnInit, OnDestroy{
-
+export class FormQuestionComponent implements OnInit, OnDestroy {
+  private destroy$: Subject<void> = new Subject<void>();
 
   public form: FormGroup;
-  constructor(){}
+  public formData: QuestionForm;
+  constructor(
+    private translate: TranslateService
+  ) { }
 
   ngOnInit(): void {
-    
+    this.initializeFormGroup();
+    this.streamDataFromJson();
   }
 
-  private initializeFormGroup(){
+  private initializeFormGroup() {
     this.form = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -25,9 +32,22 @@ export class FormQuestionComponent implements OnInit, OnDestroy{
     })
   }
 
-  public submit(){}
+  private streamDataFromJson() {
+    this.translate.stream('support.form').pipe(takeUntil(this.destroy$)).subscribe((data: QuestionForm) => {
+      this.formData = data;
+    })
+  }
+
+  public onFileSelected(event){
+
+  }
+
+  public submit() { 
+    console.log(this.form.value)
+  }
 
   ngOnDestroy(): void {
-    
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- src/app/module/support/@shared/interface/questions.interface.ts
+++ src/app/module/support/@shared/interface/questions.interface.ts
@@ -3,3 +3,12 @@ export interface QuestionsData {
   title: string
   description: string
 }
+
+export interface QuestionForm{
+  title: string
+  name: string
+  email: string
+  textArea: string
+  file: string
+  send: string
+}
