3fc9cc9c3f9313a19ce1281fd5fd71bec3cf74cf
MacsimB <macs.belousov666@gmail.com>
2024-08-18 17:35:47 +0300

    added new branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { AuthGuard } from './shared/services/uath.guard';
 import { RulesGuard } from './shared/services/rules.guard';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./module/auth/auth.module').then((m) => m.AuthModule), canActivate: [AuthGuard] },
-  { path: '', loadChildren: () => import('./module/home/home.module').then((m) => m.HomeModule), canActivate: [RulesGuard] }
+  {path: '**', redirectTo: '/error'},
+  {path: 'error', component: Error},
+  // { path: 'auth', loadChildren: () => import('./module/auth/auth.module').then((m) => m.AuthModule), canActivate: [AuthGuard] },
+  // { path: '', loadChildren: () => import('./module/home/home.module').then((m) => m.HomeModule), canActivate: [RulesGuard] }
 ];
 
 @NgModule({
